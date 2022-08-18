9a2697df09f746697520821b2b8fb67760b95655
Koji Gardiner <koji.gardiner@gmail.com>
2022-08-18 15:27:13 -0700

    Completed logic and UI

--- calc.js
+++ calc.js
@@ -30,8 +30,77 @@ function operate(op, a, b) {
     }
 }
 
-console.log(add(5, 4));
-console.log(subtract(5, 8));
-console.log(multiply(5, 4));
-console.log(divide(5, 4));
-console.log(operate("add", 5, 4));
+function updateDisplay() {
+    const display = document.querySelector(".display");
+    display.textContent = displayString;
+}
+
+function numButtonEvent(e) {
+    const button = e.target;
+
+    // If the clearDisplay flag is set, we start new entries in a fresh string
+    if (clearDisplay) {
+        displayString = button.textContent;
+        clearDisplay = false;
+    } else if (displayString === "0") {
+        if (button.textContent !== "0") {
+            displayString = button.textContent;
+        }
+    } else {
+        displayString += button.textContent;
+    }
+    updateDisplay();
+}
+
+function clearButtonEvent(e) {
+    displayString = "0";
+    firstVal = "";
+    op = "";
+    updateDisplay();
+}
+
+function opButtonEvent(e) {
+    const button = e.target;
+
+    let currOp = button.getAttribute("id");
+
+    // if we have a value and an previous operator, apply it
+    if (firstVal !== "" && (op !== "" || currOp === "equals")) {
+        let secondVal = displayString;
+        let result = operate(op, +firstVal, +secondVal);
+
+        displayString = result;
+
+        // store the result and the operator that was clicked
+        firstVal = result;
+    } else {
+        firstVal = displayString;
+    }
+
+    // if the user hit equals, we don't store the operator since the calc has been done
+    if (currOp === "equals") {
+        op = "";
+    } else {
+        op = currOp;
+    }
+
+    clearDisplay = true;    // make sure the next number pushed clears the display
+
+    updateDisplay();
+}
+
+let displayString = "0";
+let firstVal = "";
+let op = "";
+let clearDisplay = false;
+
+updateDisplay();
+
+const numButtons = document.querySelectorAll(".btn-num");
+numButtons.forEach(button => button.addEventListener("click", numButtonEvent));
+
+const clearButton = document.querySelector(".btn-clear");
+clearButton.addEventListener("click", clearButtonEvent);
+
+const opButtons = document.querySelectorAll(".btn-op");
+opButtons.forEach(button => button.addEventListener("click", opButtonEvent));
